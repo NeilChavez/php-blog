34d2de639237eecdca81e2c7a4ce0b75f45fbeb4
NeilChavez <lolochavez22@icloud.com>
2025-06-02 18:32:18 +0200

    feat: add sing up for users

---
+++ controllers/SignUpController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace controller;
+
+use model\User;
+use MVC\Router;
+
+class SignUpController
+{
+
+  static function signUp(Router $router)
+  {
+    $user = new User();
+    $errors = [];
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+      $user = new User($_POST);
+      $errors = $user->validate();
+      //check if the user already exists
+      $emailAlreadyExists = User::findBy(["email", $user->email]);
+      if ($emailAlreadyExists) {
+        $errors[] = "Email already exists.";
+      }
+      //check if the usersname is already used
+      $usernameAlreadyUsed = User::findBy(["username", $user->username]);
+      if ($usernameAlreadyUsed) {
+        $errors[] = "Username already exists.";
+      }
+      if (empty($errors)) {
+        $user->createToken();
+        $res = $user->sendVerificationEmail($user->email, $user->token);
+        if($res){
+          echo "mail sended";
+        }
+        $user->createUser();
+      }
+    }
+    $router->render("/sign-up", [
+      "errors" => $errors,
+      "user" => $user
+    ]);
+  }
+  
+  static function checkYouEmail(Router $router)
+  {
+    $router->render("/check-your-email");
+  }
+  
+  static function activateUser(Router $router)
+  {
+    $router->render("/activate-user");
+  }
+}
---
+++ models/User.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace model;
+
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use model\ActiveRecord\ActiveRecord;
+
+class User extends ActiveRecord
+{
+  public $id;
+  public $username;
+  public $avatar;
+  public $email;
+  public $password;
+  public $role;
+  public $token;
+  public $created_at;
+  public $updated_at;
+  static $table = "users";
+  static $columns = ["id", "username", "avatar", "email", "password", "role", "token", "created_at", "updated_at"];
+  static $errors = [];
+
+  public function __construct($args = [])
+  {
+    $this->id = $args["id"] ?? null;
+    $this->username = isset($args["username"]) ? trim($args["username"]) : "";
+    $this->avatar = $args["avatar"] ?? "";
+    $this->email = isset($args["email"]) ? trim($args["email"]) : "";
+    $this->password = $args["password"] ?? "";
+    $this->role = $args["role"] ?? "subscriber"; // by default subscriber
+    $this->created_at = self::now() ?? "";
+    $this->updated_at = $args["updated_at"] ?? "";
+  }
+
+  function validate(): array
+  {
+    $emailPattern = '/^\\S+@\\S+\\.\\S+$/';
+    $isValidEmail = preg_match($emailPattern, $this->email);
+    if (!$isValidEmail) {
+      self::$errors[] = "Write a valid email.";
+    }
+    if ($this->username === "") {
+      self::$errors[] = "Write a valid username.";
+    }
+    if (strlen($this->username) < 6) {
+      self::$errors[] = "Username should be at least 6 characters.";
+    }
+    if (strlen($this->password) < 6) {
+      self::$errors[] = "Password should be at least 6 characters.";
+    }
+    return self::$errors;
+  }
+  
+  function createUser()
+  {
+    $res = $this->save();
+    if ($res) {
+      $state = $this->id ? "updated" : "created";
+      header("Location: /check-your-email");
+      exit;
+    }
+  }
+
+  function createToken()
+  {
+    $token = bin2hex(random_bytes(50));
+    $this->token = $token;
+  }
+  function sendVerificationEmail($email, $token)
+  {
+    $mail = new PHPMailer(true);
+    try {
+      // Server settings
+      $mail->SMTPDebug = SMTP::DEBUG_OFF; // Set to DEBUG_SERVER for debugging
+      $mail->isSMTP();
+      $mail->Host = 'sandbox.smtp.mailtrap.io'; // Mailtrap SMTP server host 
+      $mail->SMTPAuth = true;
+      $mail->Username = USER_SMPT; // Your Mailtrap SMTP username
+      $mail->Password = PASS_SMTP; // Your Mailtrap SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
+      $mail->Port = 2525; // TCP port to connect to
+      //Recipients
+      $mail->setFrom('mail@fortest.com', "Neil Chavez"); //Sender's email and name
+      $mail->addAddress($email); // Recipient's email
+      //Content
+      $mail->isHTML(true); //Set to true if sending HTML email
+      $mail->Subject = 'Email Verification';
+      $content = "<html>";
+      $content .= "<body>";
+      $content .= "<h1>Welcome to our blog!</h1>";
+      $content .= "<p>Here you have the link to activate your user:</p>";
+      $content .= "<a style='border: 2px solid blue;' href='" . DOMAIN_NAME . "/activate-user?token=" . $token . "'>";
+      $content .= "Active you account!";
+      $content .= "</a>";
+      $content .= "</body>";
+      $content .= "</html>";
+
+      $mail->Body = $content;
+      $mail->send();
+      return true;
+    } catch (Exception $e) {
+      return false;
+    }
+  }
+}
