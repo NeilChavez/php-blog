cf8619f383355282a01e258a5441da65f9dccc94
NeilChavez <lolochavez22@icloud.com>
2025-06-27 18:18:58 -0500

    feat: add dashboard method to retrieve counts and recent activity

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -2,30 +2,53 @@
 
 namespace controller;
 
-use model\ActiveRecord\ActiveRecord;
 use model\Post;
+use model\User;
 use MVC\Router;
+use model\Comment;
+use model\Category;
 
 class PageController
 {
   static function index(Router $router)
   {
     $posts = Post::getAll();
-    $router->render("/home", [
+    $featuredPosts = array_slice($posts, 3, 3);
+    $router->render("blog/home", [
       "title" => "My blog",
-      "posts" => $posts
+      "posts" => $posts,
+      "featuredPosts" => $featuredPosts
     ]);
   }
   static function info(Router $router)
   {
-    $router->render("/info");
+    $router->render("/blog/info");
   }
   static function aboutUs(Router $router)
   {
-    $router->render("/about-us");
+    $router->render("/blog/about-us");
   }
+  
   static function dashboard(Router $router)
   {
-    $router->render("/dashboard");
+    ["posts" => $postsCount] = Post::count();
+    ["users" => $usersCount] = User::count();
+    ["comments" => $commentsCount] = Comment::count();
+    ["categories" => $categoriesCount] = Category::count();
+    $lastsPosts = Post::getAll(["limit" => 3, "order" => "created_at desc"]);
+    $lastsComments = Comment::getAll(["limit" => 3, "order" => "created_at desc"]);
+    $router->render("/dashboard/home", [
+      "postCount" => $postsCount,
+      "usersCount" => $usersCount,
+      "commentsCount" => $commentsCount,
+      "categoriesCount" => $categoriesCount,
+      "lastsPosts" => $lastsPosts,
+      "lastsComments" => $lastsComments
+    ]);
+  }
+
+  static function notAuthorized(Router $router)
+  {
+    $router->render("/blog/not-authorized");
   }
 }
